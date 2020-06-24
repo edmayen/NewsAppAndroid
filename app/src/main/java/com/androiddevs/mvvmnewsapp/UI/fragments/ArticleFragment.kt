eb91450a230f495096ad086ef412e813084940a5
edmayen <ed_mayen@hotmail.com>
2020-06-24 12:27:41 -0500

    Setting up Navigation

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/UI/fragments/ArticleFragment.kt
@@ -0,0 +1,7 @@
+package com.androiddevs.mvvmnewsapp.UI.fragments
+
+import androidx.fragment.app.Fragment
+import com.androiddevs.mvvmnewsapp.R
+
+class ArticleFragment: Fragment(R.layout.fragment_article) {
+}
